951f6658283cf542cd5efda9921c939e9af4fd6b
lundyseab <langdygood50@gmail.com>
2024-10-21 02:14:54 +0000

    update restart ssh script

--- restart_ssh_for_servers.sh
+++ restart_ssh_for_servers.sh
@@ -1,3 +1,4 @@
-for container in $(docker ps --format '{{.Names}}' | grep -E 'student_server-server[0-9]+-1'); do
+mydir="$(basename $PWD)"
+for container in $(docker ps --format '{{.Names}}' | grep -E ${mydir}'-server[0-9]+-1'); do
   docker exec "$container" service ssh restart
 done
